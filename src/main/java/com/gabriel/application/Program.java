9ca2a473bda83444cb81549904d2f2ee4b7ca168
Gabriel França <gabrielgfranca7@proton.me>
2026-07-10 19:07:25 -0300

    test: linear and binary search

--- src/main/java/com/gabriel/application/Program.java
+++ src/main/java/com/gabriel/application/Program.java
@@ -1,17 +1,20 @@
 package com.gabriel.application;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Program {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import com.gabriel.algorithms.searching.BinarySearch;
+import com.gabriel.algorithms.searching.LinearSearch;
+import com.gabriel.benchmark.Benchmark;
+import com.gabriel.utils.RandomArrayGenerator;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+public class Program {
+    public static void main(String[] args) {
+        int[] original = RandomArrayGenerator.generateSorted(1_000_000);
+
+        IO.println(Benchmark.run(
+                "Linear Search",
+                () -> LinearSearch.search(original, 754_499)));
+
+        IO.println(Benchmark.run(
+                "Binary Search",
+                () -> BinarySearch.search(original, 754_499)));
     }
 }
