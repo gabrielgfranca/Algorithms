495fd46bbe6a2c0be106577cee63ca475662ea69
Gabriel França <gabrielgfranca7@proton.me>
2026-07-16 21:03:55 -0300

    test: selection sort algorithm

--- src/main/java/com/gabriel/application/Program.java
+++ src/main/java/com/gabriel/application/Program.java
@@ -2,21 +2,30 @@ package com.gabriel.application;
 
 import com.gabriel.algorithms.searching.BinarySearch;
 import com.gabriel.algorithms.searching.LinearSearch;
+import com.gabriel.algorithms.sorting.SelectionSort;
 import com.gabriel.benchmark.Benchmark;
-import com.gabriel.utils.RandomArrayGenerator;
+import com.gabriel.utils.RandomCollectionGenerator;
+
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
-        int[] original = RandomArrayGenerator.generateSorted(1_000_000);
+        int[] sortedArray = RandomCollectionGenerator.generateSorted(1_000_000);
+        List<Integer> unsortedArray = RandomCollectionGenerator.generateList(10_000);
 
         IO.println(Benchmark.run(
                 "Linear Search",
-                () -> LinearSearch.search(original, 754_499)));
+                () -> LinearSearch.search(sortedArray, 754_499)));
         // Execution Time: 2.087 ms
 
         IO.println(Benchmark.run(
                 "Binary Search",
-                () -> BinarySearch.search(original, 754_499)));
+                () -> BinarySearch.search(sortedArray, 754_499)));
         // Execution Time: 0.182 ms
+
+        IO.println(Benchmark.run(
+                "Selection Sort",
+                () -> SelectionSort.sort(unsortedArray)));
+        // Execution Time: 34.839 ms
     }
 }
